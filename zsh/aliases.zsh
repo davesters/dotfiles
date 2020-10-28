26c050ccbbda5a6f2478a7804d4a87ea198737c7
David Corona <davesters81@gmail.com>
2020-10-27 23:36:55 -0500

    random updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,5 @@
 alias reload!='. ~/.zshrc'
 alias -r ll='ls -la'
+
+alias jdk12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
+alias jdk8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
