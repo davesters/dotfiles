a0e5c124625fd39ad26a51fdbb68a3412f668222
Davesters <davesters81@gmail.com>
2021-10-04 14:47:28 -0500

    alias and path updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,7 @@
 alias reload!='. ~/.zshrc'
 alias -r ll='ls -la'
 
-alias jdk12="export JAVA_HOME=`/usr/libexec/java_home -v 12`; java -version"
-alias jdk8="export JAVA_HOME=`/usr/libexec/java_home -v 1.8`; java -version"
+alias jdk12="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-12.0.2.jdk/Contents/Home; java -version"
+alias jdk11="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-11.0.2.jdk/Contents/Home; java -version"
+alias jdk15="export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-15.jdk/Contents/Home; java -version"
+alias jdk8="export JAVA_HOME=/Users/david/Library/Java/JavaVirtualMachines/adopt-openjdk-1.8.0_275/Contents/Home; java -version"
